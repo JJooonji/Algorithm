84fa4a173594730039a4f78cd77de5262cb05efc
JJooonji <korewa9@naver.com>
2022-07-18 11:27:05 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,54 @@
-function solution(arr1, arr2) {
-    var answer = [[]];
-    for(let i = 0; i < arr1.length; i++) {
-        answer[i] = [];//행
-        for(let j = 0; j < arr1[i].length; j++){ //열
-            answer[i][j] = arr1[i][j] + arr2[i][j]
-        }
+function solution(s){
+    // var answer = true;
+    let searchS = s.toLowerCase()
+    let searchP = searchS.split('p').length;
+    let searchY = searchS.split('y').length
+    if(searchP == searchY){
+        return true
+    } else {
+        return false
     }
-    return answer;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//     console.log('Hello Javascript')
+
+//     return answer;
 }
+
+// //문제 분석
+// - p.lengtg === y.length -> trure
+// - p===y===0 -> true
+// - 대소문자 구별하지 않음
+// //로직분리
+// - if문을 사용해서 갯수
+// - p와 y를 추출
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(arr1, arr2) {
+//     var answer = [[]];
+//     for(let i = 0; i < arr1.length; i++) {
+//         answer[i] = [];//행
+//         for(let j = 0; j < arr1[i].length; j++){ //열
+//             answer[i][j] = arr1[i][j] + arr2[i][j]
+//         }
+//     }
+//     return answer;
+// }
 
 
 //2차원배열의 문제

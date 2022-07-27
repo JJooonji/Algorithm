799be833fac1006ea224d129da3799da9479f735
JJooonji <korewa9@naver.com>
2022-07-27 17:30:33 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,60 +1,21 @@
-function solution(s){
-    // var answer = true;
-    let searchS = s.toLowerCase()
-    let searchP = searchS.split('p').length;
-    let searchY = searchS.split('y').length
-    if(searchP == searchY){
-        return true
-    } else {
-        return false
+function solution(num) {
+    // var answer = 0;
+
+    if(num === 1){
+        return 0;
     }
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-//     console.log('Hello Javascript')
-
-//     return answer;
+    for(let i=0; i<500; i++){
+        if(num % 2 ===0){
+            num = num/2
+        }else if(num===1) {
+            return i+1
+        } else {
+            num = (num*3)+1
+        }
+    }   
+    return -1;
 }
 
-// //문제 분석
-// - p.lengtg === y.length -> trure
-// - p===y===0 -> true
-// - 대소문자 구별하지 않음
-// //로직분리
-// - if문을 사용해서 갯수
-// - p와 y를 추출
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function solution(arr1, arr2) {
-//     var answer = [[]];
-//     for(let i = 0; i < arr1.length; i++) {
-//         answer[i] = [];//행
-//         for(let j = 0; j < arr1[i].length; j++){ //열
-//             answer[i][j] = arr1[i][j] + arr2[i][j]
-//         }
-//     }
-//     return answer;
-// }
-
-
-//2차원배열의 문제
-//for 문을 이용하여 i를 정해진 길이만큼 돌려주고 [i]라는 값으로 출력
-// 다시 for문을 돌려 j를 i 길이만큼 돌려준다.
-
-// answer = []
-// answer[i] = [ [], [] ] 첫번째 for문으로 행 추가
-// answer[i][j] = [ [ 4, 6 ], [ 7, 9 ] ]  두번째 for문에서 더한 열 추가/
+console.log(solution(6))
+console.log(solution(16))
+console.log(solution(626331))
